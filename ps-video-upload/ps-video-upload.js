4a4c689d16c2890e24f6fa8a4d940f5d976269b9
Deivid Fortuna <Deivid Fortuna>
2016-07-08 20:08:06 -0300

    first commit

---
+++ ps-video-upload/ps-video-upload.js
@@ -0,0 +1,56 @@
+(function (app) {
+  'use strict';
+
+  app.directive('psVideoUpload', ['WISTIA', 'toaster', function (WISTIA, toaster) {
+    return {
+      restrict: 'E',
+      templateUrl: './ps-video-upload/ps-video-upload.html',
+      scope: {
+        add: '&',
+        done: '='
+      },
+      link: function (scope, elem) {
+        scope.fail = false;
+
+        $(elem).fileupload({
+          dataType: 'json',
+          formData: {
+            project_id: WISTIA.PROJECT_ID,
+            api_password: WISTIA.API_TOKEN
+          },
+          add: function (e, data) {
+            data.url = WISTIA.UPLOAD_URL;
+            scope.progress = 0;
+            data.submit();
+          },
+          done: function (e, data) {
+            scope.$apply(function () {
+              scope.done(data.result);
+            })
+          },
+          fail: function (e, data) {
+            scope.$apply(function () {
+              var res = data.response();
+              scope.progress = 0;
+              scope.fail = true;
+              toaster.pop({
+                type: 'error',
+                title: 'Error',
+                body: res.jqXHR.responseJSON.error,
+                timeout: 5000
+              });
+            })
+          },
+          progressall: function (e, data) {
+            if (data.total > 0) {
+              scope.$apply(function () {
+                scope.progress = parseInt(data.loaded / data.total * 100, 10);
+              });
+            }
+          }
+        });
+      }
+    }
+  }]);
+
+})(angular.module('app'));
